ee2346526e2ca8bda873ff7d1d4a13923d3a7414
MLeys <102929892+MLeys@users.noreply.github.com>
2023-02-03 22:43:54 -0500

    BUILD: SubSkill page and routes

--- src/components/SkillDisplay/SkillDisplay.jsx
+++ src/components/SkillDisplay/SkillDisplay.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { 
-    Segment, Card
+    Segment,
+    Card,
+    Button,
+    Icon
 
 } from 'semantic-ui-react';
 
@@ -17,7 +21,19 @@ export default function SkillDisplay({ allSkills }) {
                     <Segment.Group>
                         <Segment textAlign='center'>{skill.name}</Segment>
                             <Segment.Group textAlign='center' horizontal>
-                            <Segment.Group><Segment.Group horizontal>ff </Segment.Group>SubSkills
+                            <Segment.Group>
+                                <Segment.Group horizontal>
+                                    <Segment>SubSkills</Segment>
+                                    <Segment>
+                                        <Button icon labelpostition='right'>
+                                            <Link to={`/${skill.name}/subskill`}>
+                                                <Icon name='plus' />
+                                            </Link>
+
+                                        </Button>
+                                    </Segment>
+                                    <Segment> Edit Subskill</Segment>
+                                </Segment.Group>
                                 <SubSkillCard skill={skill}/>
                             </Segment.Group>
                             <Segment.Group> Resourcs
---
+++ src/pages/SubSkillPage/SubSkillPage.jsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import {
+    Grid,
+    Segment,
+
+} from 'semantic-ui-react';
+
+import * as skillsApi from '../../utils/skillApi'
+export default function SubSkillPage() {
+
+
+
+
+    return (
+        <>
+            <h1>Subskill page</h1>
+        </>
+    )
+}
