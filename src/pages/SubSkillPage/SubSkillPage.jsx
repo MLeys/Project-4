2f845d58936c4981b34e9d18679982326777a416
MLeys <102929892+MLeys@users.noreply.github.com>
2023-02-05 14:36:03 -0500

    BUILD: Subskill page form for edit

--- src/pages/SubSkillPage/SubSkillPage.jsx
+++ src/pages/SubSkillPage/SubSkillPage.jsx
@@ -10,6 +10,8 @@ import {
 import * as skillsApi from '../../utils/skillApi'
 import subSkills from "../../../controllers/subSkills";
 
+import SubSkillCard from "../../components/SubSkillCard/SubSkillCard";
+
 
 export default function SubSkillPage({ loggedUser, skill, allSkills, getSkills}) {
     
@@ -22,16 +24,60 @@ export default function SubSkillPage({ loggedUser, skill, allSkills, getSkills})
     // console.log(parentSkill, "parentskill")
     const subSkills = parentSkill?.subSkills
     // console.log(subSkills, "subskills")
-    const subSkill = subSkills?.filter((sub) => sub.id === subId)
+    const subSkill = subSkills?.filter((sub) => sub._id === subId)
 
-    // console.log(parentName, subId, subParams)
-    // console.log(subSkills, "<--subSkills on subSkillPage")
+    console.log(parentName, subId, subParams)
+    console.log(subSkill, "<--subSkill on subSkillPage")
     
     
+    
+    const [state, setState] = useState({
+        name: "",
+        type: "",
+    })
 
-    return (
-        <>
-            <h1>Subskill page - {parentName} - {subId}</h1>
-        </>
-    )
+    function handleChange(e) {
+		console.log(e.target.name, "e.target.name in handleChange in Addskillform")
+        setState({
+          ...state,
+          [e.target.name]: e.target.value,
+        });
+		// console.log(state, " <<<UPDATED STATE FROM HANDLE CHANGE")
+      }
+
+    function handleSubmit(e) {
+		e.preventDefault();
+	
+		console.log(state, "<___<<<<<<<< state in handleSubmit")
+		handleAddSkill(state);
+	}
+    
+  return (
+	
+	<Segment>
+		<h1> this is inside form segment</h1>
+		<Form onSubmit={handleSubmit}>
+		<Form.Input
+			className="form-control"
+			name="name"
+			value={state.name}
+			placeholder="Enter Skill Name"
+			onChange={handleChange}
+			
+		/>
+		<Form.Input
+			className="form-control"
+			name="type"
+			value={state.type}
+			placeholder="Enter Type of Skill"
+			onChange={handleChange}
+		/>
+		<Button type="submit" className="btn">
+			Add Skill
+		</Button>
+
+		</Form>
+	</Segment> 
+
+  );
 }
