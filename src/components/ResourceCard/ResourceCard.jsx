023b1e21c843e2fefe55c051db0c3e7a2d1f1d68
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-01 11:59:26 -0500

    REFORMAT: Search for resources and display

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -22,80 +22,50 @@ function ResourceCard({liftYouTubeSearchResults, youTubeSearchResults, skill}) {
 
 	useEffect(() => {
 		getResources();
-	}, [!resources]);
+	}, [youTubeSearchResults]);
 	
 	return (
-		<>
-		{
-			resources.map((res) => {
-				console.log(res.title)
-				return (
-				  
-					<Card key={`search-${skill.name}-${res.videoId}`}>
-					  	<iframe
-						
-						  width="280"
-						  height="157.5"
-						  src={`https://www.youtube.com/embed/${res.videoId}`}
-						  title={res.title}
-						  allowFullScreen
-						/>
-					  <Card.Content>
-						<Card.Header content={res.title}/>
-						<Card.Meta content={res.publishTime} />
-						<Card.Description content={res.description} />
-					  </Card.Content>
-					  <Card.Content extra={true}>
-						<div className='ui two buttons'>
-						  <Button basic color='green'>
-							Learn
-						  </Button>
-						  <Button basic color='red'>
-							Nope
-						  </Button>
-						</div>
-					  </Card.Content>
-					</Card>
-				)
-			  })
-		}
-		
-		</>
-	)
+	<>{
+		resources.map((res) => {
+			console.log(res.title)
+			return (
+				
+				<Card key={`search-${skill.name}-${res.videoId}`}>
+					<Embed
+						autoplay={false}
+						color='white'
+						hd={false}
+						id={res.videoId}
+						iframe={{
+							allowFullScreen: true,
+							style: {
+								padding: 10,
+							},
+						}}
+						placeholder={res.thumbnail}
+						source='youtube'
+					/>
 
-    resultSearchYT.map((res) => {
-        console.log(res.title)
-        return (
-          
-            <Card key={`search-${skill.name}-${res.videoId}`}>
-              <Image src={res.thumbnail} wrapped ui={false}/>
-              <Card.Content>
-                <Card.Header content={res.title}/>
-                <Card.Meta content={res.publishTime} />
-                <Card.Description content={res.description} />
-              </Card.Content>
-              <Card.Content extra={true}>
-                <div className='ui two buttons'>
-                  <Button basic color='green'>
-                    Approve
-                  </Button>
-                  <Button basic color='red'>
-                    Decline
-                  </Button>
-                </div>
-              </Card.Content>
-              {/* <Card.Content extra={true}>
-                <iframe
-                  width="280"
-                  height="157.5"
-                  src={`https://www.youtube.com/embed/${res.videoId}`}
-                  title={res.title}
-                  allowFullScreen
-                />
-              </Card.Content> */}
-            </Card>
-        )
-      })
+					<Card.Content>
+					<Card.Header content={res.title}/>
+					<Card.Meta content={res.publishTime} />
+					<Card.Description content={res.description} />
+					</Card.Content>
+					<Card.Content extra={true}>
+					<div className='ui two buttons'>
+						<Button basic color='green'>
+						Learn
+						</Button>
+						<Button basic color='red'>
+						Nope
+						</Button>
+					</div>
+					</Card.Content>
+				</Card>
+			)
+		})
+	}</>
+	)
 }
 
 export default ResourceCard;
