b3a9f3d5450092402c2a6f36f49c8c45c067213b
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-02 12:14:22 -0500

    REFACT: SkillsReducer to switch case

--- src/reducers/SkillsReducer.jsx
+++ src/reducers/SkillsReducer.jsx
@@ -1,23 +1,28 @@
 function SkillsReducer(skills, action) {
-    if (action.type === 'createSkill') {
-        return [...skills, action.data ];
-
-    } else if (action.type === 'updateSkill') {
-        return (
-            skills.map((s) => {
-                if (s.id === action.skill.id) {
-                    return action.skill;
-                } else {
-                    return s;
-                }
-            })
-        )
-
-    } else if (action.type === 'deleteSkill') {
-        return skills.filter((s) => s.id !== action.id);
-
-    } else {
-        throw Error (`Error handling action: ${action.type}`)
+    switch (action.type) {
+        case 'createSkill': {
+            return [...skills, action.data ];
+        }
+        case 'readSkills': {
+            throw Error (`${action.type} - NOT CREATED YET`)
+        }
+        case 'updateSkill': {
+            return (
+                skills.map((s) => {
+                    if (s.id === action.skill.id) {
+                        return action.skill;
+                    } else {
+                        return s;
+                    }
+                })
+            )
+        }
+        case 'deleteSkill': {
+            return skills.filter((s) => s.id !== action.id);
+        }
+        default: {
+            throw Error (`Error handling action: ${action.type}`)
+        }
     }
 }
 
