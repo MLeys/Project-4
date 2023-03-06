bfdb0349a957d0dc0a53dd6633930d3e92e71e4a
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-06 00:02:01 -0500

    BUILD: Displaying youtube search results

--- src/components/SearchYouTube/SearchYouTube.jsx
+++ src/components/SearchYouTube/SearchYouTube.jsx
@@ -66,55 +66,6 @@ function SearchYouTube() {
 	
 	}, []);  
 
-	const DisplayResults = () =>{
-		console.log("display results")
-		{
-			results.map((resource, index) => {
-				return (
-				<Card.Group>
-					<Card 
-						key={`${resource.videoId}-${index}`}
-						onClick={(e, resource)=>{handleSelect(e, resource, index)}}
-							
-					>
-						<Embed
-							autoplay={false}
-							color='white'
-							hd={false}
-							id={resource.videoId}
-							iframe={{
-								allowFullScreen: true,
-								style: {
-									padding: 5,
-								},
-							}}
-							placeholder={resource.thumbnail}
-							source='youtube'
-						/>
-	
-						<Card.Content>
-						<Card.Header content={resource.title}/>
-						<Card.Meta content={resource.publishTime} />
-						<Card.Description content={resource.description} />
-						</Card.Content>
-						<Card.Content extra={true}>
-						<div className='ui two buttons'>
-							<Button basic color='green'>
-							Learn
-							</Button>
-							<Button basic color='red'>
-							Nope
-							</Button>
-						</div>
-						</Card.Content>
-					</Card>
-				</Card.Group>
-				)
-			})
-		}
-	}
-
-
 	return (
 		<Container>
 			<Segment.Group>
