9c17f11e8091c98100028c1f9e80135730f8caaf
MLeys <102929892+MLeys@users.noreply.github.com>
2023-04-09 12:04:02 -0400

    REFACT: Video Resource Cards

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { useContext } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
@@ -9,40 +11,58 @@ import Typography from '@mui/material/Typography';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
+import { styled } from '@mui/material/styles';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-function VideoCard() {
+import { SkillsContext } from '../../context/SkillsContext/SkillsContext';
+
+
+function VideoCard({resource, index}) {
   const theme = useTheme();
+	const ctx = useContext(SkillsContext);
+  const {title, videoId, description, thumbnail, datePublished, skillId, subSkillId } = resource;
+
+  function ResourceVideo() {
+    return (
+      <iframe
+      
+        src={`https://www.youtube.com/embed/${resource.videoId}`}
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+      /> 
+    )
+  }
 
   return (
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Mac Miller
-          </Typography>
+    <Box width='100%'>
+      <Card >
+        <CardContent>
+          <CardHeader
+            sx={{m: 0, p: 0, maxWidth: '100%'}}
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={resource.title}
+            subheader={resource.description}
+            titleTypographyProps={{ fontWeight: 900, m: 0, p: 0, alignContent: 'center', maxWidth: '100%'}}
+          />
+          <ResourceVideo />
         </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
-        </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
-    </Card>
+      </Card>
+
+    </Box>
+
   );
 }
 
