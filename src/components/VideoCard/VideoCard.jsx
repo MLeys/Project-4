efcb1dbd88c1e47432a45007aad0bc1e0bab8603
MLeys <102929892+MLeys@users.noreply.github.com>
2023-04-09 12:45:56 -0400

    REFACT: video cards

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { useContext } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions'
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -12,13 +14,13 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import { styled } from '@mui/material/styles';
-import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Badge from '@mui/material/Badge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import { SkillsContext } from '../../context/SkillsContext/SkillsContext';
@@ -32,7 +34,6 @@ function VideoCard({resource, index}) {
   function ResourceVideo() {
     return (
       <iframe
-      
         src={`https://www.youtube.com/embed/${resource.videoId}`}
         frameborder='0'
         allow='autoplay; encrypted-media'
@@ -43,20 +44,24 @@ function VideoCard({resource, index}) {
   }
 
   return (
-    <Box width='100%'>
+    <Box component={Paper} elevation={16}>
       <Card >
-        <CardContent>
-          <CardHeader
-            sx={{m: 0, p: 0, maxWidth: '100%'}}
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title={resource.title}
-            subheader={resource.description}
-            titleTypographyProps={{ fontWeight: 900, m: 0, p: 0, alignContent: 'center', maxWidth: '100%'}}
-          />
+        <CardContent sx={{my: .5, p: 0, maxWidth: '100%'}}>
+          <Box ml={2} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>            
+            <Typography variant="h5" component="div" sx={{ fontWeight: 900 }}>
+              {resource.title}
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <CardActions sx={{ alignSelf: 'flex-start', pr: 0 , mt: 0, pb: 5}}>
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              </CardActions>
+            </Box>
+          </Box>
+          <Typography variant="body1" color="secondary.contrastText" >
+            {resource.description}
+          </Typography>
           <ResourceVideo />
         </CardContent>
       </Card>
