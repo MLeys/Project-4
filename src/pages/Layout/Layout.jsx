28236f9dc8d2134534c819cd4d185d3ec42512cd
MLeys <102929892+MLeys@users.noreply.github.com>
2023-04-11 01:15:16 -0400

    REFACTor

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,19 +2,14 @@ import { Outlet } from "react-router-dom";
 import { useEffect, useContext} from "react";
 import Container from '@mui/material/Container';
 
-
-
 import { SkillsContext, SkillsDispatchContext } from '../../context/SkillsContext/SkillsContext.jsx';
 
 import FixedMenuHeader from '../../components/FixedMenuHeader/FixedMenuHeader';
-import MainFooter from '../../components/MainFooter/MainFooter';
 
 
 function Layout({ handleLogout }) {
   const ctx = useContext(SkillsContext)
   const getSkills = ctx.getSkills;
-
-
 
   useEffect(() => {
     getSkills();
@@ -22,15 +17,8 @@ function Layout({ handleLogout }) {
 
   return (
     <Container fluid='true' style={{margin: 0, padding: 0}}>
-
-  
     <FixedMenuHeader handleLogout={handleLogout} />
-
-
-          
-          <Outlet />
-
-      <MainFooter />
+      <Outlet />
     </Container> 
   )
 }
