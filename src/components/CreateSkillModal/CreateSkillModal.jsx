c4f15c89bddd9b811817cdbca1f85b2e0f665f9c
MLeys <102929892+MLeys@users.noreply.github.com>
2023-04-11 01:12:54 -0400

    REFACTor imports

--- src/components/CreateSkillModal/CreateSkillModal.jsx
+++ src/components/CreateSkillModal/CreateSkillModal.jsx
@@ -5,7 +5,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import Dialog from '@mui/materialDialog';
+import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
